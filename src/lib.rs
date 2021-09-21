f5ffedeeb21ae23191ce218b93be253a90c91674
KarasawaTakahiro <karasawa@basicinc.jp>
2021-09-21 11:18:30 +0900

    layout

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,78 @@
 use wasm_bindgen::prelude::*;
 use yew::prelude::*;
 
-struct Model {
+struct Model {}
+
+struct Paragraph {
+    value: String,
+    style: Style,
+}
+
+struct SettingPane {
     link: ComponentLink<Self>,
-    value: i64,
+    value: String,
+}
+
+#[derive(Default)]
+struct Style {
+    background_color: Option<String>,
 }
 
 enum Msg {
-    AddOne,
+    Update(String),
 }
 
-impl Component for Model {
+impl Component for Paragraph {
     type Message = Msg;
     type Properties = ();
+
+    fn create(_: Self::Properties, _link: ComponentLink<Self>) -> Self {
+        Self {
+            value: "paragraph".to_string(),
+            style: Style::default(),
+        }
+    }
+
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        true
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        // Should only return "true" if new properties are different to
+        // previously received properties.
+        // This component has no properties so we will always return "false".
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <p style=self.style()>{&self.value}</p>
+        }
+    }
+}
+
+impl Paragraph {
+    fn style(&self) -> String {
+        self.style.to_css()
+    }
+}
+
+impl Component for SettingPane {
+    type Message = Msg;
+    type Properties = ();
+
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
-        Self { link, value: 0 }
+        Self {
+            link,
+            value: "foo".to_string(),
+        }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
-            Msg::AddOne => self.value += 1,
+            Msg::Update(value) => self.value = value,
         }
+
         true
     }
 
@@ -34,8 +86,55 @@ impl Component for Model {
     fn view(&self) -> Html {
         html! {
             <div>
-                <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
-                <p>{ self.value }</p>
+                <section>
+                    <p>{ "background color" }</p>
+                    <input type="text"
+                           value=self.value.clone()
+                           oninput=self.link.callback(|e: InputData| Msg::Update(e.value))/>
+                    <p>{&self.value}</p>
+                </section>
+            </div>
+        }
+    }
+}
+
+impl Style {
+    fn to_css(&self) -> String {
+        let mut css = String::from("");
+
+        if let Some(background_color) = &self.background_color {
+            css.push_str(format!("background-color: {};", background_color).as_str());
+        };
+
+        css
+    }
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+    fn create(_: Self::Properties, _link: ComponentLink<Self>) -> Self {
+        Self {}
+    }
+
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        true
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        // Should only return "true" if new properties are different to
+        // previously received properties.
+        // This component has no properties so we will always return "false".
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <div>
+                <div>
+                    <Paragraph/>
+                </div>
+                <SettingPane/>
             </div>
         }
     }
